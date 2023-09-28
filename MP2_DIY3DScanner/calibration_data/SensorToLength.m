dd6b7d5b288f3cc1fddf57003b069d3f205b3988
Jun Park <cpark1@olin.edu>
2023-09-28 00:14:55 -0400

    Adding more plots

--- MP2_DIY3DScanner/calibration_data/SensorToLength.m
+++ MP2_DIY3DScanner/calibration_data/SensorToLength.m
@@ -8,6 +8,12 @@ m_20 = load("Calibration_20in.mat","m");
 m_30 = load("Calibration_30in.mat","m");
 m_50 = load("Calibration_50in.mat","m");
 
+%Load Extra Sensor Data for distances of 6,13,25,45 inches
+m_6 = load("Calibration_6in.mat","m");
+m_13 = load("Calibration_13in.mat","m");
+m_25 = load("Calibration_25in.mat","m");
+m_45 = load("Calibration_45in.mat","m");
+
 %Find the average output for each distance
 avg_05in = mean(m_5.m(:,1));
 avg_10in = mean(m_10.m(:,1));
@@ -15,29 +21,53 @@ avg_20in = mean(m_20.m(:,1));
 avg_30in = mean(m_30.m(:,1));
 avg_50in = mean(m_50.m(:,1));
 
+avg_06in = mean(m_6.m(:,1));
+avg_13in = mean(m_13.m(:,1));
+avg_25in = mean(m_25.m(:,1));
+avg_45in = mean(m_45.m(:,1));
+
+%Initial Data Points
 sensor_voltage = [avg_05in,avg_10in,avg_20in,avg_30in,avg_50in];
-length = [5,10,20,30,50];
+initial_lengths = [5,10,20,30,50];
+
+%Extra Data Points for Error
+other_sensor_voltage = [avg_06in,avg_13in,avg_25in,avg_45in];
+other_lengths = [6,13,25,45];
 
 % Plotting Original Data
-scatter(sensor_voltage,length,'b','*')
+hold on
+scatter(sensor_voltage,initial_lengths,'b','*')
 
 % Polynomial fitting to derive transfer function
-P = polyfit(sensor_voltage,length,3);
+P = polyfit(sensor_voltage,initial_lengths,3);
 sensor_fit = polyval(P,sensor_voltage);
 eqn = string("Length = " + P(1)) + "*SensorData + " + string(P(2));
 
 %Plotting Line Fit
-hold on;
-plot(sensor_voltage,sensor_fit,'r-.');
+plot(sensor_voltage,sensor_fit,'g-.');
 
-% Plotting estimates using transfer function
+xlabel("Sensor Analog Voltage Readings (V)")
+ylabel("Distance (in)")
+legend("Measured Data","Polynomial Fit Curve")
+title("Calibration Plot of Analog Voltage vs Actual Distance")
+hold off
+
+hold on
+%Plotting inital & extra values for error evaluation
+scatter(sensor_voltage,initial_lengths,'b','*')
+scatter(other_sensor_voltage,other_lengths,'r','*')
+
+% Plotting estimates using transfer function 
 test_range = 0:10:800;
 estimate_length=transfer_equation(test_range,P);
+hold on
 plot(test_range,estimate_length)
 
-xlabel("Sensor Data")
-ylabel("Length")
-legend("Data","Fit","Estimate")
+xlabel("IR Sensor Analog Data (V)")
+ylabel("Distance (in)")
+title("Error Plot")
+
+legend("Initial Data","Extra Data for Error Measure","Estimated Curve")
 hold off
 
 function vals = transfer_equation(x,P)
